e25c3d6944c08cf8f007c5ab3b2bac42219c2ae9
Ovidiu <ovi_nba_and1@yahoo.com>
2016-01-27 23:14:27 +0200

    Added addSurvey validation

--- src/main/java/ro/tpjad/service/impl/SurveyServiceImpl.java
+++ src/main/java/ro/tpjad/service/impl/SurveyServiceImpl.java
@@ -2,12 +2,15 @@ package ro.tpjad.service.impl;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ro.tpjad.dao.SurveyDAO;
+import ro.tpjad.entity.Question;
+import ro.tpjad.entity.QuestionType;
 import ro.tpjad.entity.Survey;
 import ro.tpjad.entity.SurveyException;
 import ro.tpjad.service.SurveyService;
@@ -21,18 +24,38 @@ public class SurveyServiceImpl implements SurveyService {
 	@Override
 	@Transactional
 	public void addSurvey(Survey survey) throws SurveyException {
+		List<Question> questions = survey.getQuestions();
+		if (StringUtils.isBlank(survey.getName())) {
+			throw new SurveyException("Please enter a name for survey!");
+		}
+		if (StringUtils.isBlank(survey.getCreateUser())) {
+			throw new SurveyException("Please enter your name!");
+		}
+		if (questions.isEmpty()) {
+			throw new SurveyException("Please add some questions!");
+		} else {
+			for (Question question : questions) {
+				if (StringUtils.isBlank(question.getText())) {
+					throw new SurveyException("Please add text question!");
+				} else if (question.getType() != QuestionType.FREE_TEXT
+						&& StringUtils.isBlank(question.getPossibleAnswers())) {
+					throw new SurveyException("Please add answers to "
+							+ question.getText() + " !");
+				}
+			}
+		}
 		surveyDao.addSurvey(survey);
 	}
 
 	@Override
 	@Transactional
-	public void deleteSurvey(Survey survey) throws SurveyException{
+	public void deleteSurvey(Survey survey) throws SurveyException {
 		surveyDao.deleteSurvey(survey);
 	}
 
 	@Override
 	@Transactional(readOnly = true)
-	public List<Survey> getAllSurveys() throws SurveyException{
+	public List<Survey> getAllSurveys() throws SurveyException {
 		List<Survey> surveys = surveyDao.getAllSurveys();
 		System.out.println("getAllSurveys Service: " + surveys);
 		for (Survey survey : surveys) {
@@ -43,7 +66,7 @@ public class SurveyServiceImpl implements SurveyService {
 
 	@Override
 	@Transactional(readOnly = true)
-	public Survey findSurvey(Long id) throws SurveyException{
+	public Survey findSurvey(Long id) throws SurveyException {
 		Survey survey = surveyDao.findSurvey(id);
 		if (survey != null) {
 			Hibernate.initialize(survey.getQuestions());
